fc4db020864dc962821cd9c6ebaff750fc49ae45
Mmx <green.dusk8264@fastmail.com>
2024-09-13 00:37:08 +0800

    feat: enhance backoff pkg

--- pkg/backoff/backoff.go
+++ pkg/backoff/backoff.go
@@ -3,41 +3,57 @@ package backoff
 import (
 	"context"
 	"github.com/Mmx233/BackoffCli/tools"
+	log "github.com/sirupsen/logrus"
 	"sync/atomic"
 	"time"
 )
 
 // Backoff 错误重试 积分退避算法
 type Backoff struct {
-	retry time.Duration
-
-	conf    Conf
+	Config  Conf
 	running *atomic.Bool
 }
 
 type Conf struct {
-	Content func(ctx context.Context) error
+	Fn     func(ctx context.Context) error
+	Logger *log.Logger
 
-	// MaxRetryDelay maximum retry wait time, default 20 minutes
-	MaxRetryDelay time.Duration
-	// RetryGrowthFactor determines how many times the retry time doubles, default 1
-	RetryGrowthFactor int
+	// InitialDuration initial wait time, default 1 second
+	InitialDuration time.Duration
+	// MaxDuration maximum retry wait time, default 20 minutes
+	MaxDuration time.Duration
+
+	// $Next = ($Last + InterConstFactor) * (2 ^ ExponentFactor) + OuterConstFactor
+
+	// ExponentFactor default 1
+	ExponentFactor   int
+	InterConstFactor time.Duration
+	OuterConstFactor time.Duration
 }
 
+// New backoff instance with default values
 func New(c Conf) Backoff {
-	if c.Content == nil {
+	if c.InitialDuration == 0 {
+		c.InitialDuration = time.Second
+	}
+	if c.MaxDuration == 0 {
+		c.MaxDuration = time.Minute * 20
+	}
+	if c.ExponentFactor <= 0 {
+		c.ExponentFactor = 1
+	}
+	return NewInstance(c)
+}
+
+func NewInstance(c Conf) Backoff {
+	if c.Fn == nil {
 		panic("content function required")
 	}
-	if c.MaxRetryDelay == 0 {
-		c.MaxRetryDelay = time.Minute * 20
+	if c.Logger == nil {
+		c.Logger = log.New()
 	}
-	if c.RetryGrowthFactor <= 0 {
-		c.RetryGrowthFactor = 1
-	}
-
 	return Backoff{
-		retry:   time.Second,
-		conf:    c,
+		Config:  c,
 		running: &atomic.Bool{},
 	}
 }
@@ -50,14 +66,13 @@ func (a Backoff) Start(ctx context.Context) error {
 	return &ErrorAlreadyRunning{}
 }
 
-// Worker
-// 请注意,此处使用的是普通接收器,当 worker 重新运行时 retry 会被重置
 func (a Backoff) Worker(ctx context.Context) {
+	wait := a.Config.InitialDuration
 	for {
 		errChan := make(chan error)
 		go func() {
 			defer tools.Recover()
-			errChan <- a.conf.Content(ctx)
+			errChan <- a.Config.Fn(ctx)
 		}()
 		if err := <-errChan; err == nil {
 			break
@@ -66,14 +81,14 @@ func (a Backoff) Worker(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			return
-		case <-time.After(a.retry):
+		case <-time.After(wait):
 			// continue retry
 		}
 
-		if a.retry < a.conf.MaxRetryDelay {
-			a.retry = a.retry << a.conf.RetryGrowthFactor
-			if a.retry > a.conf.MaxRetryDelay {
-				a.retry = a.conf.MaxRetryDelay
+		if wait < a.Config.MaxDuration {
+			wait = (wait+a.Config.InterConstFactor)<<a.Config.ExponentFactor + a.Config.OuterConstFactor
+			if wait > a.Config.MaxDuration {
+				wait = a.Config.MaxDuration
 			}
 		}
 	}
