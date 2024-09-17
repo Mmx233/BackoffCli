2e36c1107583e63d2980c706086c0cc9431566d5
Mmx <green.dusk8264@fastmail.com>
2024-09-18 02:08:43 +0800

    feat: implement regular health checker

--- backoff/health.go
+++ backoff/health.go
@@ -1,18 +1,57 @@
 package backoff
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
-type HealthChecker struct {
-	Config HealthCheckerConfig
-}
-
-type HealthCheckerConfig struct {
+type RegularHealthCheckerConfig struct {
 	CheckInterval    time.Duration
 	InitialDelay     time.Duration
 	SuccessThreshold int
 	FailureThreshold int
 }
 
-func NewHealthChecker() {
+func NewRegularHealthChecker(fn func(ctx context.Context) error, conf RegularHealthCheckerConfig) func(ctx context.Context) <-chan error {
+	return func(ctx context.Context) <-chan error {
+		errChan := make(chan error, 1)
+		var success, failure int
+		go func() {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(conf.CheckInterval):
+			}
 
+			time.Sleep(conf.InitialDelay)
+			ticker := time.NewTicker(conf.CheckInterval)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ctx.Done():
+					errChan <- ctx.Err()
+					return
+				case <-ticker.C:
+					err := fn(ctx)
+					if err != nil {
+						failure++
+						if failure >= conf.FailureThreshold {
+							errChan <- err
+							return
+						}
+						success = 0
+					} else {
+						success++
+						if success >= conf.SuccessThreshold {
+							errChan <- nil
+							return
+						}
+						failure = 0
+					}
+				}
+			}
+		}()
+		return errChan
+	}
 }
