ae971048aa8a40a7c78432df728dcb098f987295
Mmx <green.dusk8264@fastmail.com>
2024-09-22 01:36:34 +0800

    feat: probe health checker unit test

--- backoff/health_check_test.go
+++ backoff/health_check_test.go
@@ -5,11 +5,74 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
+func TestProbeHealthChecker_Success(t *testing.T) {
+	t.Parallel()
+
+	var count atomic.Uint32
+	errChan := NewProbeHealthChecker(func(ctx context.Context) error {
+		count.Add(1)
+		return nil
+	}, ProbeHealthCheckerConfig{
+		CheckInterval:    time.Millisecond * 2,
+		InitialDelay:     time.Millisecond,
+		SuccessThreshold: 5,
+	})(context.Background())
+
+	if count.Load() != 0 {
+		t.Fatal("initial delay not work")
+	}
+	time.Sleep(time.Millisecond * 2)
+	if count.Load() != 1 {
+		t.Fatal("first fn call not work")
+	}
+
+	select {
+	case err := <-errChan:
+		if err != nil {
+			t.Fatalf("expected nil error, got %v", err)
+		}
+		if count.Load() != 5 {
+			t.Fatalf("success threshold not work properly, expected 5, got %d", count.Load())
+		}
+	case <-time.After(time.Millisecond * 15):
+		t.Fatal("timeout")
+	}
+}
+
+func TestProbeHealthChecker_Failure(t *testing.T) {
+	t.Parallel()
+
+	var count atomic.Uint32
+	expectedErr := errors.New("test error")
+	errChan := NewProbeHealthChecker(func(ctx context.Context) error {
+		count.Add(1)
+		return expectedErr
+	}, ProbeHealthCheckerConfig{
+		CheckInterval:    time.Millisecond,
+		FailureThreshold: 5,
+	})(context.Background())
+
+	select {
+	case err := <-errChan:
+		if !errors.Is(err, expectedErr) {
+			t.Fatalf("expected error: '%v', got: '%v'", expectedErr, err)
+		}
+		if count.Load() != 5 {
+			t.Fatalf("failure threshold not work properly, expected 5, got %d", count.Load())
+		}
+	case <-time.After(time.Millisecond * 10):
+		t.Fatal("timeout")
+	}
+}
+
 func TestHttpProbeHealthCheckFn_Request(t *testing.T) {
+	t.Parallel()
+
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/health" {
 			rw.WriteHeader(http.StatusOK)
@@ -27,6 +90,8 @@ func TestHttpProbeHealthCheckFn_Request(t *testing.T) {
 }
 
 func TestHttpProbeHealthCheckFn_Response_Check(t *testing.T) {
+	t.Parallel()
+
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/health" {
 			_, _ = rw.Write([]byte("ok"))
@@ -74,6 +139,8 @@ func TestHttpProbeHealthCheckFn_Response_Check(t *testing.T) {
 }
 
 func TestHttpProbeHealthCheckFn_Timeout(t *testing.T) {
+	t.Parallel()
+
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		time.Sleep(time.Second)
 	}))
